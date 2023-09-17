9fcc6fc25f0d61c8174141cddce6ab334feca0fd
leke <lekeyemo@gmail.com>
2023-09-17 10:29:11 +0100

    second commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -27,35 +27,34 @@ function Card({ data, percentage }) {
             data-testid="movie-poster"
           />
         </div>
-      </Link>
 
-      <Link to={`/movies/${data.id}`}>
+        <p data-testid="movie-released-date" className="mt-3 mb-3">
+          {data.release_date}
+        </p>
         <h3 data-testid="movie-title" className="mt-3">
           {data.title}
         </h3>
+
+        <div className="d-flex justify-content-between mt-3">
+          <div className="d-flex gap-2 align-item-center">
+            <Image
+              src="https://res.cloudinary.com/dbxvk3apv/image/upload/v1694595577/cofeshop/MV5BMTk3ODA4Mjc0NF5BMl5BcG5nXkFtZTgwNDc1MzQ2OTE_1_qaz2ip.webp"
+              alt="imdb image"
+              className="imdb-image"
+            />
+            <p className="mb-0 small-text ">
+              {data.vote_average.toFixed(2)}/10
+            </p>
+          </div>
+          <div className="d-flex gap-2 align-item-center">
+            <Image
+              src="https://res.cloudinary.com/dbxvk3apv/image/upload/v1694596461/cofeshop/PngItem_1381056_1_1_fkoyqh.png"
+              alt="imdb image"
+            />
+            <p className="mb-0 small-text ">{percentage()}%</p>
+          </div>
+        </div>
       </Link>
-
-      <div className="d-flex justify-content-between mt-3">
-        <div className="d-flex gap-2 align-item-center">
-          <Image
-            src="https://res.cloudinary.com/dbxvk3apv/image/upload/v1694595577/cofeshop/MV5BMTk3ODA4Mjc0NF5BMl5BcG5nXkFtZTgwNDc1MzQ2OTE_1_qaz2ip.webp"
-            alt="imdb image"
-            className="imdb-image"
-          />
-          <p className="mb-0 small-text ">{data.vote_average.toFixed(2)}/10</p>
-        </div>
-        <div className="d-flex gap-2 align-item-center">
-          <Image
-            src="https://res.cloudinary.com/dbxvk3apv/image/upload/v1694596461/cofeshop/PngItem_1381056_1_1_fkoyqh.png"
-            alt="imdb image"
-          />
-          <p className="mb-0 small-text ">{percentage()}%</p>
-        </div>
-      </div>
-
-      <p data-testid="movie-released-date" className="mt-3">
-        {data.release_date}
-      </p>
     </div>
   );
 }
